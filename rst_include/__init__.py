87936335672643b7eec7709c1a7026b60bc3243d
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-25 23:00:12 +0200

    Version 1.0.0 - public release

--- rst_include/__init__.py
+++ rst_include/__init__.py
@@ -3,8 +3,8 @@ __title__ = 'rst_include'
 __version__ = '1.0.0'
 __name__ = 'rst_include'
 
-from .libs.lib_classes import RstFile
-from .libs.lib_classes import RstConf
-from .libs.lib_main import rst_str_replace
-from .libs.lib_main import rst_inc_from_config
-from .libs.lib_main import rst_inc
+from rst_include.libs.lib_classes import RstFile as RstFile
+from rst_include.libs.lib_classes import RstConf as RstConf
+from rst_include.libs.lib_main import rst_str_replace as rst_str_replace
+from rst_include.libs.lib_main import rst_inc_from_config as rst_inc_from_config
+from rst_include.libs.lib_main import rst_inc as rst_inc
