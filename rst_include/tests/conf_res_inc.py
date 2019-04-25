e0accb3f0aefee18acc9924283e1c844d625ae2a
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-25 20:37:50 +0200

    Version 1.0.0 - public release

--- rst_include/tests/conf_res_inc.py
+++ rst_include/tests/conf_res_inc.py
@@ -5,16 +5,15 @@ import rst_include.libs.lib_classes as lib_classes
 # set config here
 rst_conf = lib_classes.RstConf()
 
-rst_conf.l_rst_files = [
-    lib_classes.RstFile(source='test1_no_includes_template.rst',
-                        target='test1_no_includes_result.rst',
-                        source_encoding='utf-8-sig',  # default = utf-8-sig because it can read utf-8 and utf-8-sig
-                        target_encoding='utf-8'      # default = utf-8
-                        ),
-    lib_classes.RstFile(source='test2_include_samedir_template.rst',
-                        target='test2_include_samedir_result.rst'),
-    lib_classes.RstFile(source='test3_include_subdir_template.rst',
-                        target='test3_include_subdir_result.rst'),
-    lib_classes.RstFile(source='test4_include_nocode_template.rst',
-                        target='test4_include_nocode_result.rst'),
-    ]
+rst_conf.l_rst_files = [lib_classes.RstFile(source='test1_no_includes_template.rst',
+                                            target='test1_no_includes_result.rst',
+                                            # default = utf-8-sig because it can read utf-8 and utf-8-sig
+                                            source_encoding='utf-8-sig',
+                                            # default = utf-8
+                                            target_encoding='utf-8'),
+                        lib_classes.RstFile(source='test2_include_samedir_template.rst',
+                                            target='test2_include_samedir_result.rst'),
+                        lib_classes.RstFile(source='test3_include_subdir_template.rst',
+                                            target='test3_include_subdir_result.rst'),
+                        lib_classes.RstFile(source='test4_include_nocode_template.rst',
+                                            target='test4_include_nocode_result.rst')]
