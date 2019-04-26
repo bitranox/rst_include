8577fb00fc3cc65cd9bad25cb5f9fac62c504986
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-27 01:30:21 +0200

    refractor

--- build_readme.py
+++ build_readme.py
@@ -14,9 +14,9 @@ def project_specific(repository_slug, repository, repository_dashed):
     # PROJECT SPECIFIC
     logger = logging.getLogger('project_specific')
     logger.info('create help documentation files')
-    os.system('"{sys_executable} -m ./rst_inc.py -h > ./docs/rst_include_help_output.txt"'.format(sys_executable=sys.executable))
-    os.system('"{sys_executable} -m ./rst_inc.py include -h > ./docs/rst_include_help_include_output.txt"'.format(sys_executable=sys.executable))
-    os.system('"{sys_executable} -m ./rst_inc.py replace -h > ./docs/rst_include_help_replace_output.txt"'.format(sys_executable=sys.executable))
+    os.system('"{sys_executable} -m rst_include/rst_inc.py -h > ./docs/rst_include_help_output.txt"'.format(sys_executable=sys.executable))
+    os.system('"{sys_executable} -m rst_include/rst_inc.py include -h > ./docs/rst_include_help_include_output.txt"'.format(sys_executable=sys.executable))
+    os.system('"{sys_executable} -m rst_include/rst_inc.py replace -h > ./docs/rst_include_help_replace_output.txt"'.format(sys_executable=sys.executable))
 
 
 def parse_args(cmd_args=sys.argv[1:]):
