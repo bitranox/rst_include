08a77fcabebe5d6ddcc6dc1a01800857b4125b52
Robert Nowotny <bitranox@users.noreply.github.com>
2019-04-27 18:33:57 +0200

    refractor

--- rst_include/libs/lib_classes.py
+++ rst_include/libs/lib_classes.py
@@ -1,12 +1,9 @@
-from typing import Union
-
-
 class RstFile(object):
     def __init__(self, source: str, target: str, source_encoding: str = 'utf-8-sig', target_encoding: str = 'utf-8') -> None:
-        self.source: str = source
-        self.source_encoding: str = source_encoding
-        self.target: str = target
-        self.target_encoding: str = target_encoding
+        self.source = source
+        self.source_encoding = source_encoding
+        self.target = target
+        self.target_encoding = target_encoding
 
 
 class RstConf(object):
@@ -16,26 +13,26 @@ class RstConf(object):
 
 class SourceLine(object):
     def __init__(self, line_number: int = 0, content: str = ''):
-        self.line_number: int = line_number
-        self.content: str = content
+        self.line_number = line_number
+        self.content = content
 
 
 class Block(object):
     def __init__(self, source_file_name: str):
-        self.is_include_block: bool = False
-        self.source_file_name: str = source_file_name
-        self.l_source_lines: [SourceLine] = []
+        self.is_include_block = False
+        self.source_file_name = source_file_name
+        self.l_source_lines = []
 
-        self.include_filename: str = ''
-        self.include_filename_absolut: str = ''
-        self.include_file_code: str = ''
-        self.include_file_encoding: str = 'utf-8-sig'
-        self.include_file_start_line: Union[str, None] = None
-        self.include_file_end_line: Union[int, None] = None
-        self.include_file_start_after: str = ''
-        self.include_file_end_before: str = ''
-        self.include_file_lines: [str] = list()
-        self.include_file_sliced_content: [str] = ''
-        self.include_file_number_of_blanks_to_add_to_content: int = 0
-        self.pass_through_options: [SourceLine] = list()
-        self.additional_content: [SourceLine] = list()
+        self.include_filename = ''
+        self.include_filename_absolut = ''
+        self.include_file_code = ''
+        self.include_file_encoding = 'utf-8-sig'
+        self.include_file_start_line = None  # Union[str, None]
+        self.include_file_end_line = None  # Union[int, None]
+        self.include_file_start_after = ''
+        self.include_file_end_before = ''
+        self.include_file_lines = list()
+        self.include_file_sliced_content = ''
+        self.include_file_number_of_blanks_to_add_to_content = 0
+        self.pass_through_options = list()  # [SourceLine]
+        self.additional_content = list()    # [SourceLine]
