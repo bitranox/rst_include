fc1195eb061fb60850cb980c927c4381d26d7367
Robert Nowotny <bitranox@users.noreply.github.com>
2019-05-09 09:58:03 +0200

    initial

--- rst_include/rst_inc.py
+++ rst_include/rst_inc.py
@@ -1,25 +1,12 @@
 import errno
 import os
 import sys
-"""
-from rst_include import *
-from rst_include.libs import lib_log
-from rst_include.libs import lib_args
-from rst_include.libs import lib_test
-from rst_include.libs import lib_test_compare_results
-"""
-try:
-    from . import *
-    from .libs import lib_log
-    from .libs import lib_args
-    from .libs import lib_test
-    from .libs import lib_test_compare_results
-except (ModuleNotFoundError, ImportError):  # pragma: no cover
-    from libs import *
-    from libs import lib_log
-    from libs import lib_args
-    from libs import lib_test
-    from libs import lib_test_compare_results
+
+from . import *
+from .libs import lib_log
+from .libs import lib_args
+from .libs import lib_test
+from .libs import lib_test_compare_results
 
 
 def handle_include_command(argparse_namespace, sys_argv):
