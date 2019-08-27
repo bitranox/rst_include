8c8f6c8eb05899fa951bc4c3fe4347643ced6492
bitranox <rnowotny1966@gmail.com>
2019-08-27 20:26:17 +0200

    refractor

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
 # this __init__.py is only meant for local package development
-from .rst_include import *
+try:
+    from .rst_include import *
+# this we need for pip install --install-option test
+except ImportError:
+    import rst_include
--- rst_include/__init__.py
+++ rst_include/__init__.py
@@ -1,4 +1,12 @@
+import errno                            # this we need tor main() in __main__.py probably for commandline entry point - to check
+import sys                              # this we need tor main() in __main__.py probably for commandline entry point - to check
+import logging                          # this we need tor main() in __main__.py probably for commandline entry point - to check
 import pathlib
+
+from .libs.lib_classes import RstFile
+from .libs.lib_classes import RstConf
+from .libs.lib_main import rst_str_replace
+from .libs.lib_main import rst_inc
 
 
 def get_version() -> str:
@@ -10,11 +18,3 @@ def get_version() -> str:
 __title__ = 'rst_include'
 __version__ = get_version()
 __name__ = 'rst_include'
-
-import errno                            # this we need tor main() in __main__.py probably for commandline entry point - to check
-import sys                              # this we need tor main() in __main__.py probably for commandline entry point - to check
-import logging                          # this we need tor main() in __main__.py probably for commandline entry point - to check
-from .libs.lib_classes import RstFile
-from .libs.lib_classes import RstConf
-from .libs.lib_main import rst_str_replace
-from .libs.lib_main import rst_inc
