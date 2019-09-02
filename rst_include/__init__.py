7f6293b94fce3639c9ad779111d8c75e3b6bc5ed
bitranox <rnowotny1966@gmail.com>
2019-09-02 22:45:59 +0200

    refractor

--- rst_include/__init__.py
+++ rst_include/__init__.py
@@ -1,9 +1,7 @@
-# import errno                            # this we need tor main() in __main__.py probably for commandline entry point - to check
-# import sys                              # this we need tor main() in __main__.py probably for commandline entry point - to check
-# import logging                          # this we need tor main() in __main__.py probably for commandline entry point - to check
+# STDLIB
 import pathlib
 
-
+# PROJECT
 from .libs.lib_classes import RstFile
 from .libs.lib_main import rst_str_replace
 from .libs.lib_main import rst_inc
